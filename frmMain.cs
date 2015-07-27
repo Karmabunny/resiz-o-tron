debcf967894614044607ac4c3ae06b12b978eef0
Josh Heidenreich <josh.sickmate@gmail.com>
2015-07-27 12:42:16 +0930

    Thumbnail generation was crashing upon invalid files

--- frmMain.cs
+++ frmMain.cs
@@ -70,12 +70,17 @@ namespace ReallyEasyResize
         {
             foreach (ListViewItem it in lstQueue.Items) {
                 if (it.ImageIndex == -1) {
-                    Bitmap orig = new Bitmap((string)it.Tag);
-                    Bitmap icon = doScale(orig, imgQueue.ImageSize.Width, imgQueue.ImageSize.Height);
-                    orig.Dispose();
-                    imgQueue.Images.Add(icon);
-                    it.ImageIndex = imgQueue.Images.Count - 1;
+                    try {
+                        Bitmap orig = new Bitmap((string)it.Tag);
+                        Bitmap icon = doScale(orig, imgQueue.ImageSize.Width, imgQueue.ImageSize.Height);
+                        orig.Dispose();
+                        imgQueue.Images.Add(icon);
+                        it.ImageIndex = imgQueue.Images.Count - 1;
+                    } catch {
+                        lstQueue.Items.Remove(it);
+                    }
 
+                    // Only process a max of one image per timer tick
                     return;
                 }
             }
